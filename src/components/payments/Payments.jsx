48b92d3a917226a94854ab123db85cccbfb89004
Ricardo Serrano <ricardo.ss@live.com>
2022-06-09 16:05:27 -0500

    feat: apply corret space betweeb table elements

--- src/components/payments/Payments.jsx
+++ src/components/payments/Payments.jsx
@@ -65,70 +65,70 @@ const Payments = () => {
           <h4>Latest transactions</h4>
           <ul>
             <li>
-              <p className="time">Today</p>
+              <p className="time">02.Jun</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category red">Food</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">01.Mar</p>
               <p className="description">Playera manga larga ojoi  asdoij</p>
               <p className="payment">Cash</p>
               <p className="category green">Clothes</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">16.Dec</p>
               <p className="description">Queso</p>
-              <p className="payment">Cash</p>
+              <p className="payment">Debit Card</p>
               <p className="category blue">Entreteiment</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">24.Oct</p>
               <p className="description">Queso</p>
-              <p className="payment">Cash</p>
+              <p className="payment">Bank Transfer</p>
               <p className="category pink">Health</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">16.Sep</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category cyan">Salary</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">03.Jan</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category orange">Education</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">24.Feb</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category brown">Other</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">18.May</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category purple">Bills</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">12.Jul</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category purple">Food</p>
               <p className="total"> -$15,00  </p>
             </li>
             <li>
-              <p className="time">Today</p>
+              <p className="time">02.Nov</p>
               <p className="description">Queso</p>
               <p className="payment">Cash</p>
               <p className="category purple">Food</p>
