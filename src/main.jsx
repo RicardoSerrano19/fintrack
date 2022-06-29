1aaa0098a1f4284228ff632e49e188f2cc7902e2
Ricardo Serrano <ricardo.ss@live.com>
2022-06-29 17:54:04 -0500

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Payments from './components/payments/Payments'
+import Home from './components/home/Home'
+import Deposits from './components/deposits/Deposits'
+import History from './components/history/History'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="payments" element={<Payments />} />
+          <Route path="deposits" element={<Deposits />} />
+          <Route path="history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
