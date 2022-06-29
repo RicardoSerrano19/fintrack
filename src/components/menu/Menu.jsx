ee849b9c859d1e6b0ca05a98160b311172f34351
Ricardo Serrano <ricardo.ss@live.com>
2022-06-29 17:53:47 -0500

    feat: add navlink to menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,17 +1,40 @@
 import { Home, Send, Pocket,Trello, CreditCard, Calendar } from 'react-feather'
 import Logo from '../../svg/dibujo-3.svg'
 import Favicon from '../../svg/favicon.svg'
+import { Link, NavLink } from 'react-router-dom'
+
 const Menu = () => {
   return (
     <nav>
         <img src={Logo} className='logo' alt="Fintrack" width={'50%'}/>
         <img src={Favicon} className='favicon' alt="Fintrack" width={'100%'}/>
         <ul>
-            <li className='active'><Home /> <span>Home</span></li>
-            <li><Send /> <span>Payments</span></li>
+            <NavLink
+                className={({ isActive }) => isActive ? "active" : ""}
+                to="/">
+              <li><Home /> <span>Home</span></li>
+            </NavLink>
+
+            <NavLink 
+                className={({ isActive }) => isActive ? "active" : ""} 
+                to="/payments">
+              <li><Send /> <span>Payments</span></li>
+            </NavLink>
+
             <li><Calendar /> <span>Calendar</span></li>
-            <li><Pocket /> <span>Deposits</span></li>
-            <li><Trello /> <span>History</span></li>
+
+            <NavLink 
+                className={({ isActive }) => isActive ? "active" : ""}
+                to="/deposits">
+              <li><Pocket /> <span>Deposits</span></li>
+            </NavLink>
+
+            <NavLink 
+                className={({ isActive }) => isActive ? "active" : ""}
+                to="/history">
+              <li><Trello /> <span>History</span></li>
+            </NavLink>
+
             <li><CreditCard /> <span>Accounts</span></li>
         </ul>
     </nav>
